6fff6a88db9d4a0851602d9a240ea63532200fa1
iMadfxq <imadfxq@gmail.com>
2022-08-02 07:35:49 -0400

    Added button to shuffle cards and re start turn count. Event listeners

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import { useEffect } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 let CARDS = [
-  {content: '🔥'},
-  {content: '👍🏼'},
-  {content: '💯'},
-  {content: '💰'},
-  {content: '⏳'},
-  {content: '🚲'},
-
-]
-
+  { content: "🔥" },
+  { content: "👍🏼" },
+  { content: "💯" },
+  { content: "💰" },
+  { content: "⏳" },
+  { content: "🚲" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0)
+
   const cardShuffler = () => {
-    CARDS = [...CARDS, ...CARDS]
+    let shuffledCards = [...CARDS, ...CARDS]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+
+    setTurns(0)
+  };
+
+  const plusTurn = () => {
+    setTurns((turns) => 
+      turns + 1
+    )
   }
-  useEffect(()=>{
-    cardShuffler()
-    console.log(CARDS)
-  }, [])
-  return;
+
+  useEffect(() => {
+    cardShuffler();
+  }, []);
+
+  return (
+    <div>
+      {cards.map((card) => {
+      return (
+      <div key={card.id} onClick={plusTurn} >{card.content}</div>
+      )
+    })}
+    <p>{turns}</p>
+    <button onClick={cardShuffler}>New game</button>
+    </div>
+  );
 }
 
 export default App;
